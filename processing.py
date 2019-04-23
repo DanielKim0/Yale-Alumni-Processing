5b2b6dd435d1ace49242c13fd5a295aa8c04416f
Daniel Kim <danielynskim@gmail.com>
2019-04-23 17:15:53 -0400

    Update with CSVs

--- processing.py
+++ processing.py
@@ -18,8 +18,8 @@ def get_attendances(attendances):
 
 def parse_file(file):
     data = pd.read_csv(file)
-    attendances = data[["email", "event"]]
-    data.pop("event")
+    attendances = data[["email", "events"]]
+    data.pop("events")
 
     attendances, uniques = get_attendances(attendances)
     attendances = pd.DataFrame(attendances)
@@ -27,22 +27,40 @@ def parse_file(file):
     events = pd.DataFrame({"name": list(uniques)})
     events = events.assign(month="", year="", description="", CLY_sponsored="", location="")
 
-    if not os.path.exists("results"):
-        os.makedirs("results")
+    results = os.path.join(os.path.join(os.path.expanduser('~'), 'Downloads'), "results")
+    if not os.path.exists(results):
+        os.makedirs(results)
 
-    data.to_csv(os.path.join("results", "alumni.csv"), index = False)
-    events.to_csv(os.path.join("results", "events.csv"), index = False)
-    attendances.to_csv(os.path.join("results", "attendances.csv"), index = False)
+    data.to_csv(os.path.join(results, "alumni.csv"), index = False)
+    events.to_csv(os.path.join(results, "events.csv"), index = False)
+    attendances.to_csv(os.path.join(results, "attendances.csv"), index = False)
 
 def UploadAction(event=None):
     filename = filedialog.askopenfilename()
     parse_file(filename)
-    label.config(text="Success!")
+    label.config(text="Successfully Converted CSV Files.")
+
+def AlumnusAction(event=None):
+    file = open(os.path.join(os.path.join(os.path.expanduser('~'), 'Downloads'), "alumnus_template.csv"), "w")
+    file.write("first_name,last_name,email,phone,location,college,yale_degree,other_degrees,linkedin,employer,employed_field,recommender,description")
+    file.close()
+    label.config(text="Successfully Downloaded Alumnus Template.")
+
+def StudentAction(event=None):
+    file = open(os.path.join(os.path.join(os.path.expanduser('~'), 'Downloads'), "student_template.csv"), "w")
+    file.write("first_name,last_name,email,year")
+    file.close()
+    label.config(text="Successfully Downloaded Student Template.")
 
 root = tk.Tk()
-button = tk.Button(root, text='Open', command=UploadAction, height=5, width=20)
+root.geometry("500x300")
+button = tk.Button(root, text='Convert Alumnus Template', command=UploadAction, height=5, width=50)
+alumnus_button = tk.Button(root, text='Download Alumnus Template', command=AlumnusAction, height=5, width=50)
+student_button = tk.Button(root, text='Download Student Template', command=StudentAction, height=5, width=50)
 label = tk.Label(root, text="Upload a CSV file containing\na comma-delimited column with events.")
 button.pack()
+alumnus_button.pack()
+student_button.pack()
 label.pack()
 
 root.mainloop()
